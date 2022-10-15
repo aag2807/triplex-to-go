045f5374395ea231aee547f98c1fb27cb223c0ec
Alvaro Guzman <aagn2807@gmail.com>
2022-10-14 21:34:36 -0400

    v0.0.2 of release completed

--- state.go
+++ state.go
@@ -4,21 +4,24 @@ import "reflect"
 
 type State struct{}
 
-func (s *State) IsTrue(arg bool, errormsg string) bool {
-	if !arg {
+// IsTrue panics if stateQuery is false.
+func (s *State) IsTrue(stateQuery bool, errormsg string) bool {
+	if !stateQuery {
 		panic(errormsg)
 	}
 	return true
 }
 
-func (s *State) IsFalse(arg bool, errormsg string) bool {
-	if arg {
+// IsFalse panics if stateQuery is true.
+func (s *State) IsFalse(stateQuery bool, errormsg string) bool {
+	if stateQuery {
 		panic(errormsg)
 	}
 
 	return true
 }
 
+// Equals verifies deep equal between both arguments, will panic if they dont match.
 func (s *State) Equals(arg1, arg2 interface{}, errormsg string) bool {
 	if arg1 != arg2 || !reflect.DeepEqual(arg1, arg2) {
 		panic(errormsg)
